b53b580bc5811535ee91d04964b3525376c52ae6
ronjones340 <ej07696@gmail.com>
2020-04-13 14:09:35 +0300

    *Initial*

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,12 @@
+from timetype import days,weeks,months
+
 def estimator(data):
-  return data
+  ptype = data['periodType']
+  if (ptype == 'days'):
+    results = days(data)
+  elif (ptype == 'weeks'):
+    results = weeks(data)
+  else:
+    results = months(data) 
+  return results
+
