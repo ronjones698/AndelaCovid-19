d000668bb1b45646f29b4cb26bfacbd6d4bfc188
ronjones340 <ej07696@gmail.com>
2020-04-13 14:13:49 +0300

    *FIRST*

---
+++ timetype.py
@@ -0,0 +1,43 @@
+from math import trunc
+
+def days(data):
+    impact = {}
+    severeImpact = {}
+    impact['currentlyInfected'] = data['reportedCases'] * 10
+    severeImpact['currentlyInfected'] = data['reportedCases'] * 50
+    impact['infectionsByRequestedTime'] = impact['currentlyInfected'] * pow(2, trunc(data['timeElapsed'] / 3))
+    severeImpact['infectionsByRequestedTime'] = severeImpact['currentlyInfected'] * pow(2, trunc(data['timeElapsed'] / 3))
+    result = {
+        'data': data,
+        'impact': impact,
+        'severeImpact':severeImpact
+    }
+    return result
+
+def weeks(data):
+    impact = {}
+    severeImpact = {}
+    impact['currentlyInfected'] = data['reportedCases'] * 10
+    severeImpact['currentlyInfected'] = data['reportedCases'] * 50
+    impact['infectionsByRequestedTime'] = impact['currentlyInfected'] * pow(2, trunc(data['timeElapsed'] *7 / 3))
+    severeImpact['infectionsByRequestedTime'] = severeImpact['currentlyInfected'] * pow(2, trunc(data['timeElapsed']*7/ 3))
+    result = {
+        'data': data,
+        'impact': impact,
+        'severeImpact':severeImpact
+    }
+    return result
+
+def months(data):
+    impact = {}
+    severeImpact = {}
+    impact['currentlyInfected'] = data['reportedCases'] * 10
+    severeImpact['currentlyInfected'] = data['reportedCases'] * 50
+    impact['infectionsByRequestedTime'] = impact['currentlyInfected'] * pow(2, trunc(data['timeElapsed']*30/ 3))
+    severeImpact['infectionsByRequestedTime'] = severeImpact['currentlyInfected'] * pow(2, trunc(data['timeElapsed']*30/ 3))
+    result = {
+        'data': data,
+        'impact': impact,
+        'severeImpact':severeImpact
+    }
+    return result
